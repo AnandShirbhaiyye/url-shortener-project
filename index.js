d8da6386323f1c0e65ae9b9376fe537c91ae4e2d
AnandShirbhaiyye <anandshirbhaiyye@gmail.com>
2023-07-02 15:42:31 +0530

    create basic structure of table

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 import express from "express";
 const app = express();
 
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res)=>{
+  res.render('index')
+})
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
