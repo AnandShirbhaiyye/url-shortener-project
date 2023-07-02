9aca0821b317a809214814148d479faa1cc08f34
AnandShirbhaiyye <anandshirbhaiyye@gmail.com>
2023-07-02 16:46:35 +0530

    create model of shorturl

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
 const app = express();
+app.use(express.json());
+
+
+dotenv.config();
+
+async function connectMongoDB() {
+  const conn = await mongoose.connect(process.env.MONGODB_URL);
+  if (conn) {
+    console.log("Connected to MongoDB");
+  }
+}
+connectMongoDB();
 
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res)=>{
   res.render('index')
+})
+
+app.post('/shortUrls',(req,res)=>{
+
 })
 
 const PORT = process.env.PORT || 5000;
